1117b042a5315ecf77da09fbeb4bd95fa01028cb
ThaoNP <npthao1312@gmail.com>
2021-04-07 14:36:37 +0700

    Edit comment on main

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+# Bài 2: Bạn là nhân viên thanh toán tại cửa hàng. Khách hàng muốn đổi tiền mặt. Viết chương trình để tính làm sao bạn có thể trả lại số tờ tiền ít nhất có thể. Sử dụng tiền tệ Việt Nam {1, 2, 5, 10, 20, 50, 100, 200, 500}.
+# Exercise 2: You’re a cashier. Customers want to change their money. Write a program to minimize the number of cash you’re dispensing for each customer. Using Vietnamese currency {1, 2, 5, 10, 20, 50, 100, 200, 500}.
+
 def main(number):
     pass
